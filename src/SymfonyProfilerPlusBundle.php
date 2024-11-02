9696e94f1b4aabc849d86c0379a74a16b31060b6
Jessie UGOLIN <jessie.ugolin@gmail.com>
2024-11-02 16:16:09 +0100

    showing icon in toolbar

--- src/SymfonyProfilerPlusBundle.php
+++ src/SymfonyProfilerPlusBundle.php
@@ -3,7 +3,12 @@
 namespace Mister6mik\SymfonyProfilerPlus;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class SymfonyProfilerPlusBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+    }
 }
